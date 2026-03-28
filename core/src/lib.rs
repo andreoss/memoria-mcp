ffa2c37d457c0985c8d6994de6d87fdc7ab73c3d
andreoss <andreoss@sdf.org>
2026-03-28 21:59:09 +0000

    refactor: remove stale dead-code suppressions

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -14,18 +14,3 @@ pub use error::CoreError;
 
 #[cfg(test)]
 pub(crate) mod test_support;
-
-#[must_use]
-pub const fn placeholder() -> bool {
-    true
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn placeholder_returns_true() {
-        assert!(placeholder());
-    }
-}
